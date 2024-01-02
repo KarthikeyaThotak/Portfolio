7768a8d4b5f9ce0448cfbea592b4fb3c8f29b213
KarthikeyaThotak <karthikeya.gvm@gmail.com>
2024-01-02 03:54:32 -0500

    Changed the Quote on the Home section

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
 
             <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
-                text="Turning vision into reality with code and design."
+                text="Code may be elegant, but security is its true beauty."
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
